3b055f5643aed50c15fafbddfc93cdf3082f768a
Roman <w.romankornev@gmail.com>
2016-08-02 23:50:43 +0300

    [FIX] Unknown word translation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import webbrowser
-from textblob import TextBlob, Word
+from textblob import TextBlob, exceptions
 from wox import Wox, WoxAPI
 
 LANGUAGE = 'ru'
@@ -10,16 +10,19 @@ def translate(query):
     en = set(chr(i) for i in range(ord('a'), ord('z') + 1))
     results = []
     if query_modified:
-        from_lang, to_lang = ('en', LANGUAGE) if query_modified[0] in en else (LANGUAGE, 'en')
-        translation = TextBlob(query_modified).translate(from_lang, to_lang)
-        results.append({
-            "Title": str(translation),
-            "SubTitle": query,
-            "IcoPath":"Images/app.png",
-            "JsonRPCAction":{'method': 'openUrl',
-                             'parameters': [r'http://translate.google.com/#{}/{}/{}'.format(from_lang, to_lang, query)],
-                             'dontHideAfterAction': False}
-        })
+        try:
+            from_lang, to_lang = ('en', LANGUAGE) if query_modified[0] in en else (LANGUAGE, 'en')
+            translation = TextBlob(query_modified).translate(from_lang, to_lang)
+            results.append({
+                "Title": str(translation),
+                "SubTitle": query,
+                "IcoPath":"Images/app.png",
+                "JsonRPCAction":{'method': 'openUrl',
+                                 'parameters': [r'http://translate.google.com/#{}/{}/{}'.format(from_lang, to_lang, query)],
+                                 'dontHideAfterAction': False}
+            })
+        except exceptions.NotTranslated:
+            pass
     if not results:
         results.append({
                 "Title": 'Not found',
